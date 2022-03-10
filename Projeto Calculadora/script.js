fbe382444d5b6c5757aa482523137dc3f07e26b1
Marcelo Garofalo <89047714+marcelombg@users.noreply.github.com>
2022-03-10 10:48:41 -0300

    Calculadora

---
+++ Projeto Calculadora/script.js
@@ -0,0 +1,25 @@
+function insert (num){
+    
+    const numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+
+}
+
+function clean () {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back () {
+    const resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
+
+function calcular () {
+    const resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('resultado').innerHTML = "Sem cálculo"
+    }
+}
